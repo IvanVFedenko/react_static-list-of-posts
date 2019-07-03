5e9e2b5dd9f42b2d53caf2b39e61406e3355e5d4
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-07-03 17:20:34 +0300

    Updates

---
+++ precache-manifest.559589c1a846242cb1790b98d1d0cfa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b295180ac7a28819e4f02ca2d5e9701a",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "d7d4664821e562886953",
+    "url": "/react_static-list-of-posts/static/css/main.e177edee.chunk.css"
+  },
+  {
+    "revision": "b92c19612304e1fb6f48",
+    "url": "/react_static-list-of-posts/static/js/2.328f4660.chunk.js"
+  },
+  {
+    "revision": "d7d4664821e562886953",
+    "url": "/react_static-list-of-posts/static/js/main.ba173034.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
